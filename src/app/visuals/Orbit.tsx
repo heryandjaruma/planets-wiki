dad1409714f056f1b9795189968f19d853808852
Heryan Djaruma <heryandjaruma@gmail.com>
2024-10-05 13:58:04 +0700

    proper side

--- src/app/visuals/Orbit.tsx
+++ src/app/visuals/Orbit.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as THREE from 'three';
 
 
 interface OrbitProps {
@@ -10,7 +11,7 @@ const Orbit = ({ innerRadius, outerRadius }: OrbitProps) => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]}>
       <ringGeometry args={[innerRadius, outerRadius, 64]} />
-      <meshBasicMaterial color="gray" side="double" />
+      <meshBasicMaterial color="gray" side={THREE.DoubleSide} />
     </mesh>
   );
 };
