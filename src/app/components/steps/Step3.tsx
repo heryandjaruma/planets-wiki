337f67b1339a4582641370414705bbdadcb482e6
Heryan Djaruma <heryandjaruma@gmail.com>
2024-10-06 11:39:51 +0700

    escape cahracter

--- src/app/components/steps/Step3.tsx
+++ src/app/components/steps/Step3.tsx
@@ -12,9 +12,9 @@ export default function Step3() {
         <p className="text-lg">Learn about Celestial Bodies in our Solar System</p>
         <div className="h-10"></div>
         <div className="w-3/4">
-          <p>Earth is the only known planet that supports life, thanks to its perfect location in the "Goldilocks zone," where conditions are just right for liquid water. Its atmosphere protects us from harmful solar radiation while providing the oxygen and resources life needs to thrive. Despite challenges like climate change, Earth's ecosystems are resilient, offering hope that with care and sustainable practices, we can protect and restore the planet for future generations to enjoy its beauty and diversity.</p>
+          <p>Earth is the only known planet that supports life, thanks to its perfect location in the "Goldilocks zone," where conditions are just right for liquid water. Its atmosphere protects us from harmful solar radiation while providing the oxygen and resources life needs to thrive. Despite challenges like climate change, Earth&apos;s ecosystems are resilient, offering hope that with care and sustainable practices, we can protect and restore the planet for future generations to enjoy its beauty and diversity.</p>
           <br />
-          <p>Earth's relationship with the Moon plays a crucial role in sustaining life. The Moon's gravitational pull stabilizes Earth's tilt, which helps regulate the climate by moderating seasonal variations. It also creates ocean tides, essential for marine ecosystems and the circulation of nutrients. Together, Earth and the Moon form a unique partnership that has shaped the conditions for life on our planet, reminding us of the delicate balance that makes Earth so special.</p>
+          <p>Earth&apos;s relationship with the Moon plays a crucial role in sustaining life. The Moon&apos;s gravitational pull stabilizes Earth&apos;s tilt, which helps regulate the climate by moderating seasonal variations. It also creates ocean tides, essential for marine ecosystems and the circulation of nutrients. Together, Earth and the Moon form a unique partnership that has shaped the conditions for life on our planet, reminding us of the delicate balance that makes Earth so special.</p>
         </div>
         
         <div className="py-6">
--- src/app/components/steps/Step4.tsx
+++ src/app/components/steps/Step4.tsx
@@ -14,7 +14,7 @@ export default function Step4() {
         <div className="w-3/4">
         <p>Mercury is the smallest and closest planet to the Sun, with a surface that experiences extreme temperature swings. During the day, temperatures can soar to about 430°C (800°F), but at night, they drop to as low as -180°C (-290°F). Despite being so close to the Sun, Mercury has no significant atmosphere to retain heat, leading to these drastic shifts.</p>
         <br />
-        <p>The planet's surface is covered with craters and ridges, similar to our Moon, a testament to its ancient history of impacts. Mercury also has a weak magnetic field, which is surprising for a small planet with a slow rotation. Although it's barren and inhospitable, Mercury's resilience to the Sun's harsh environment provides scientists with valuable insights into the formation and evolution of rocky planets.</p>
+        <p>The planet&apos;s surface is covered with craters and ridges, similar to our Moon, a testament to its ancient history of impacts. Mercury also has a weak magnetic field, which is surprising for a small planet with a slow rotation. Although it&apos;s barren and inhospitable, Mercury&apos;s resilience to the Sun&apos;s harsh environment provides scientists with valuable insights into the formation and evolution of rocky planets.</p>
         </div>
         
         <div className="py-6">
