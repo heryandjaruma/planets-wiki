2f076e60e33d77656d1a494d2343c4e984242a34
Heryan Djaruma <heryandjaruma@gmail.com>
2024-10-06 11:43:32 +0700

    add missing

--- src/app/components/steps/Step1.tsx
+++ src/app/components/steps/Step1.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import useSettingsStore from "@/stores/useSettingsStore";
 import Link from "next/link";
 
-export default function() {
+export default function Step1() {
   const { currentStep, setCurrentStep } = useSettingsStore();
   
   return (
